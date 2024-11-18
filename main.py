6bb1f206272dcbfc5238b8cb12c3069a487c99bf
Emma Pedersen <epede24@wgu.edu>
2024-11-18 17:30:35 -0600

    Implemented data_split step

--- main.py
+++ main.py
@@ -63,7 +63,6 @@ def go(config: DictConfig):
                      "max_price": config['etl']['max_price']
                  },
             )
-            pass
 
         if "data_check" in active_steps:
             _ = mlflow.run(
@@ -77,13 +76,18 @@ def go(config: DictConfig):
                     "max_price": config["etl"]["max_price"]
                 },
             )
-            pass
 
         if "data_split" in active_steps:
-            ##################
-            # Implement here #
-            ##################
-            pass
+            _ = mlflow.run(
+                f"{config['main']['components_repository']}/train_val_test_split",
+                'main',
+                parameters = {
+                    "input":"clean_sample.csv:latest",
+                    "test_size": config["modeling"]["test_size"],
+                    "random_seed": config["modeling"]["random_seed"], 
+                    "stratify_by": config["modeling"]["stratify_by"]
+                }
+            )
 
         if "train_random_forest" in active_steps:
 
